dfe8e4b5a67d5b01baeca4a8283f92b78f1a0384
esrgioo <esrgioox@gmail.com>
2024-08-06 03:12:11 -0700

    feat: add register

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,3 +1,13 @@
-export const authUser = () => {
-    
+import { NextFunction, Request, Response } from 'express';
+import { registerService } from '@/services/auth/register.service';
+
+export class AuthController {
+  async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await registerService(req.body);
+      return res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
